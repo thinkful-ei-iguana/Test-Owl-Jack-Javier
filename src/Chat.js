40c0c2c4c19889cb9fdf15dcecb2c1425988e91d
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-11-12 12:45:13 -0500

    working on participants, console.loging the information from participants

---
+++ src/Chat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Chat({id,type,timestamp}){
+    console.log(id,type,timestamp)
+
+    return (
+        <section>
+            
+
+        </section>
+
+    )
+}
+
---
+++ src/Participant.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Particpant({key,name,avatar,inSession,onStage}){
+    console.log(key,name,avatar,inSession,onStage)
+
+    return null
+}
